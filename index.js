64e29ba31247705da62886ca619e4b74740c244c
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-04-28 09:19:27 +0600

    server side fully done

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-app.use(cors())
+app.use(cors());
 //! catagories data
-const catagories = require('./data/catagories.json')
+const catagories = require("./data/catagories.json");
+//! news data
+const news = require("./data/NewsData.json");
 
-app.get('/', (req, res)=>{
-  res.send('the dragon is runnig')
-})
+app.get("/", (req, res) => {
+  res.send("the dragon is runnig");
+});
 
-app.get('/catagories', (req,res)=>{
-  res.send(catagories)
-})
+app.get("/news", (req, res) => {
+  res.send(news);
+});
 
-app.listen(port, ()=>{
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedID = news.find((n) => n._id === id);
+  res.send(selectedID);
+});
+
+app.get("/catagories", (req, res) => {
+  res.send(catagories);
+});
+
+app.get("/catagories/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id === 0) {
+    res.send(news);
+  } else {
+    const selectCatagories = news.filter(
+      (ns) => parseInt(ns.category_id) === id
+    );
+    res.send(selectCatagories);
+  }
+});
+
+app.listen(port, () => {
   console.log(`Dragon API is running on ${port}`);
-})
+});
